95d962151cba2e6a4bfe710b5e7c42b4e256e73f
Srikanth <srikanth.shukrith@gmail.com>
2021-02-24 20:12:12 +0530

    Update MavenGit.java
    added comments to test the Azure yml

--- src/main/java/com/mavendemo/MavenGit.java
+++ src/main/java/com/mavendemo/MavenGit.java
@@ -3,6 +3,7 @@ package com.mavendemo;
 public class MavenGit {
 	public static void main(String args[]) {
 		System.out.println("Hello Maven im in git");
+		//adding comment to test the pipeline
 	}
 
 }
