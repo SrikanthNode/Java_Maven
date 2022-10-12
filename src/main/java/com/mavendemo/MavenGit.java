2de81daf6f3141d4394745059130419c21c647b6
Srikanth <srikanth.shukrith@gmail.com>
2022-10-12 21:38:17 +0530

    Update MavenGit.java

--- src/main/java/com/mavendemo/MavenGit.java
+++ src/main/java/com/mavendemo/MavenGit.java
@@ -4,6 +4,8 @@ public class MavenGit {
 	public static void main(String args[]) {
 		System.out.println("Hello Maven im in git, you should pull me to update in local");
 		System.out.println("Hello Maven im in git, you should pull me to update in local");
+		System.out.println("My code, Ranga");
+		System.out.println("My code, Manju");
 		//adding comment to test the pipeline
 	}
 
